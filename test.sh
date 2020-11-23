d87dac0c8328c8a5ebe7cd043a11cc27468b60aa
Bossun Wang <vvmodouco@gmail.com>
2020-11-23 16:07:41 +0800

    change data path

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
-python mask_the_face.py --path '../face_dataset/NIST_SD32_MEDS_II_face_crop/' --mask_type 'surgical' --write_path '../face_dataset/NIST_SD32_MEDS_II_face_crop_mask/' --color '#000000' --color_weight 0.75
-python mask_the_face.py --path '../face_dataset/NIST_SD32_MEDS_II_face_crop/' --mask_type 'surgical' --write_path '../face_dataset/NIST_SD32_MEDS_II_face_crop_mask/' --color '#FFFFFF' --color_weight 0.75
-python mask_the_face.py --path '../face_dataset/NIST_SD32_MEDS_II_face_crop/' --mask_type 'surgical' --write_path '../face_dataset/NIST_SD32_MEDS_II_face_crop_mask/' --color '#85C1E9' --color_weight 0.75
-python mask_the_face.py --path '../face_dataset/NIST_SD32_MEDS_II_face_crop/' --mask_type 'cloth' --write_path '../face_dataset/NIST_SD32_MEDS_II_face_crop_mask/' --color '#000000' --color_weight 0.75
-python mask_the_face.py --path '../face_dataset/NIST_SD32_MEDS_II_face_crop/' --mask_type 'cloth' --write_path '../face_dataset/NIST_SD32_MEDS_II_face_crop_mask/' --color '#FFFFFF' --color_weight 0.75
-python mask_the_face.py --path '../face_dataset/NIST_SD32_MEDS_II_face_crop/' --mask_type 'cloth' --write_path '../face_dataset/NIST_SD32_MEDS_II_face_crop_mask/' --color '#85C1E9' --color_weight 0.75
+python mask_the_face.py --path '../face_dataset/ms1m_align_112/' --mask_type 'surgical' --write_path '../face_dataset/ms1m_align_112_mask/' --color '#000000' --color_weight 0.75
+python mask_the_face.py --path '../face_dataset/ms1m_align_112/' --mask_type 'surgical' --write_path '../face_dataset/ms1m_align_112_mask/' --color '#FFFFFF' --color_weight 0.75
+python mask_the_face.py --path '../face_dataset/ms1m_align_112/' --mask_type 'surgical' --write_path '../face_dataset/ms1m_align_112_mask/' --color '#85C1E9' --color_weight 0.75
+python mask_the_face.py --path '../face_dataset/ms1m_align_112/' --mask_type 'cloth' --write_path '../face_dataset/ms1m_align_112_mask/' --color '#000000' --color_weight 0.75
+python mask_the_face.py --path '../face_dataset/ms1m_align_112/' --mask_type 'cloth' --write_path '../face_dataset/ms1m_align_112_mask/' --color '#FFFFFF' --color_weight 0.75
+python mask_the_face.py --path '../face_dataset/ms1m_align_112/' --mask_type 'cloth' --write_path '../face_dataset/ms1m_align_112_mask/' --color '#85C1E9' --color_weight 0.75
